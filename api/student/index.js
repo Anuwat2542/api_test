fe05825d643e1da08b95bac45dbfeae2669fc7e8
phukongka <aphukong@gmail.com>
2019-07-25 16:21:15 +0700

    init2

--- api/student/index.js
+++ api/student/index.js
@@ -1,17 +1,2 @@
 const express = require("express");
-//const router = express.Router();
-//module.exports = router;
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const knex = require("knex")({
-  client: "mysql",
-  connection: {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "test"
-  }
-});
-const app = express();
-const port = 7001;
+
